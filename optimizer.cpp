5dd16120917149bac8c211a22493e869b36b950a
nicejojo12 <nicejojo12@gmail.com>
2018-07-31 21:01:39 -0700

    Making optimizer functional (though it's really slow)

--- optimizer.cpp
+++ optimizer.cpp
@@ -1,11 +1,8 @@
 #include <random>
 
 class Optimizer {
-	static const kWidth = 200;
-	static const kHeight = 100;
-
 public:
-	static float error(const vector<pair<Grid, Grid> >& data, const ImageProcessor& climber) {
+	static float getError(const vector<pair<Grid, Grid> >& data, const ImageProcessor& climber) {
 		float result = 0;
 		for (int i = 0; i < data.size(); i++) {
 			result += climber.error(data[i].first, data[i].second);
@@ -14,23 +11,37 @@ public:
 	}
 
 	// data is a vector of <correct, noisy> pairs in that order
-	static climb(const vector<pair<Grid, Grid> >& data, ImageProcessor* climber) {
-		float error = error(data, *climber);
+	static void climb(const vector<pair<Grid, Grid> >& data, ImageProcessor* climber) {
+		climber->verifyItsNotTotallyBroken();
+		assert(climber != NULL);
+		float error = getError(data, *climber);
 		for (int failures = 0; failures < 500; failures++) {
+			cout << "Failures = " << failures << endl;
+			cout << "Error = " << error << endl;
+			climber->verifyItsNotTotallyBroken();
 			ImageProcessor newClimber(*climber);
-			newClimber.mutate();
-			newClimber.mutate();
-			newClimber.mutate();
-			newClimber.mutate();
-			newClimber.mutate();
-			newClimber.mutate();
-			newClimber.mutate();
-			float newError = error(data, newClimber);
+			for (int i = 0; i <= failures%20; i++) {
+				newClimber.mutate();
+			}
+			float newError = getError(data, newClimber);
+			if (0 == failures) {
+				cout << "Correct Image:" << endl;
+				data[0].first.printHeatmap();
+				cout << "Attempt at Correct Image:" << endl;
+				Grid attemptAtCorrectImage(data[0].first);
+				newClimber.process(data[0].second, &attemptAtCorrectImage);
+				attemptAtCorrectImage.printHeatmap();
+			}
 			if (error > newError) {
 				error = newError;
-				failures = 0;
-				*climber = newClimber;
+				failures = -1;
+				climber->verifyItsNotTotallyBroken();
+				newClimber.verifyItsNotTotallyBroken();
+				// *climber = newClimber;
+				climber->copyFrom(newClimber);
+				climber->verifyItsNotTotallyBroken();
 			}
+			climber->verifyItsNotTotallyBroken();
 		}
 	}
 };
---
+++ optimizer.h
@@ -0,0 +1,4 @@
+#ifndef OPTIMIZER_H
+#define OPTIMIZER_H
+#include "optimizer.cpp"
+#endif
